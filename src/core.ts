fc3bef3f29bb41bd8aa7f9689515123bd1bb7926
tech-royal <tech@royalsportz.com>
2023-01-28 23:56:53 +0530

    changed contracts

--- src/core.ts
+++ src/core.ts
@@ -1,31 +1,92 @@
 import { BigInt } from '@graphprotocol/graph-ts';
-import { EnteredContest } from '../generated/schema';
-import { EnterContest } from '../generated/templates/Matches/Matches';
+import {
+  EnteredContest as EnteredContestSchema,
+  ClaimRewards as ClaimRewardsSchema,
+  Transaction,
+} from '../generated/schema';
+import {
+  EneteredContest,
+  ClaimFunds,
+} from '../generated/templates/Matches/Matches';
 
-export function handleEnterContest(event: EnterContest): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = EnteredContest.load(
+export function handleEneteredContest(event: EneteredContest): void {
+  let entity = EnteredContestSchema.load(
     event.transaction.hash.toHex() + '-' + event.logIndex.toString()
   );
 
   // Entities only exist after they have been saved to the store;
   // `null` checks allow to create entities on demand
   if (entity == null) {
-    entity = new EnteredContest(
+    entity = new EnteredContestSchema(
       event.transaction.hash.toHex() + '-' + event.logIndex.toString()
     );
   }
 
-  // BigInt and BigDecimal math are supported
-  // entity.i = event.params.id;
-  entity.player = event.params.player;
+  let transaction = Transaction.load(
+    event.transaction.hash.toHex() + '-' + event.logIndex.toString()
+  );
+  if (transaction === null) {
+    transaction = new Transaction(
+      event.transaction.hash.toHex() + '-' + event.logIndex.toString()
+    );
+    transaction.wallet = event.address.toHexString();
+    transaction.timestamp = event.block.timestamp;
+    transaction.transactionHash = event.transaction.hash.toHexString();
+    transaction.transactionEvent = 'entered';
+  } else {
+    transaction.transactionEvent = 'entered';
+  }
+
+  entity.user = event.params.user;
   entity.amount = event.params.amount;
   entity.timestamp = event.block.timestamp;
   entity.team = event.params.team;
-  entity.matchAddress = event.address.toHexString();
-  // Entity fields can be set based on event parameters
+  entity.matchAddress = event.params.contest;
+  entity.indexedMatchToken = event.params.indexedMatchToken;
+  entity.transactionHash = event.transaction.hash.toHexString();
 
   // Entities can be written to the store with `.save()`
   entity.save();
+  transaction.enterContest = entity.id;
+  transaction.save();
+}
+
+export function handleClaimFunds(event: ClaimFunds): void {
+  let entity = ClaimRewardsSchema.load(
+    event.transaction.hash.toHex() + '-' + event.logIndex.toString()
+  );
+
+  // Entities only exist after they have been saved to the store;
+  // `null` checks allow to create entities on demand
+  if (entity == null) {
+    entity = new ClaimRewardsSchema(
+      event.transaction.hash.toHex() + '-' + event.logIndex.toString()
+    );
+  }
+
+  let transaction = Transaction.load(
+    event.transaction.hash.toHex() + '-' + event.logIndex.toString()
+  );
+  if (transaction === null) {
+    transaction = new Transaction(
+      event.transaction.hash.toHex() + '-' + event.logIndex.toString()
+    );
+    transaction.wallet = event.address.toHexString();
+    transaction.timestamp = event.block.timestamp;
+    transaction.transactionHash = event.transaction.hash.toHexString();
+    transaction.transactionEvent = 'claimed';
+  } else {
+    transaction.transactionEvent = 'claimed';
+  }
+
+  entity.user = event.params.user;
+  entity.amount = event.params.amount;
+  entity.timestamp = event.block.timestamp;
+  entity.contest = event.params.contests;
+  entity.matchToken = event.params.matchToken;
+  entity.transactionHash = event.transaction.hash.toHexString();
+  // Entities can be written to the store with `.save()`
+  entity.save();
+  transaction.claim = entity.id;
+  transaction.save();
 }
